d3b6fd40ff2a53137c3ed126ed998369577aa0b7
Carjen123 <cj@inovum.at>
2026-06-18 12:20:19 +0200

    Update Supabase URL and key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://rpxbsbmwylhyswxcuqiz.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_lTYxezkwaRozXF86IpRFqA_e9s7mQG2'
+const SUPABASE_URL = 'https://spbeckfnpmwnzyvdqyqx.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_z4-zdbfP4nlt1GLQhHdJrQ_x6fWarw1'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
